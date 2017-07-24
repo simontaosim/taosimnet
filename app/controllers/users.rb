0611b486ba66dc1ddb65ef32de5b7ffdfa6727e1
Simon 徐 <simon@sc.10086.cn.defaultbadlist>
2017-07-24 11:38:14 +0800

    adjust routes

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -19,8 +19,8 @@ Taosimnet::App.controllers :users do
   #   'Hello world!'
   # end
 
-  get :index do
-
+  get :good do
+    redirect_to url(:account, :name => "John", :id => 5)
   end
 
   get :me do
@@ -37,8 +37,47 @@ Taosimnet::App.controllers :users do
   end
 
   post :save_draft do
-    puts params[:post].to_json
+    post = Post.where(title: params[:post][:post_title]).first
+    if post.nil?
+      post = Post.new
+    end
+    post.title = params[:post][:post_title]
+    post.body = params[:post][:post_text]
+    user = User.find(session[:userId])
+    post.user = user
+    post.save
+    version = Version.where(basic_post: post.id).first
+    if version.nil?
+      version = Version.new
+      version.basic_post = post
+      version.name = post.title+"草稿"
+      version.status = "draft"
+      version.post = post
+      version.save
+    end
+    result = {
+      post_id: post.id.to_s,
+      post_version: version.id.to_s
+    }
+    result.to_json
+
+
+    # post.save!
+
+
+    # version = Version.new
+    # version.name =
+
   end
+
+  get :posts, :map => '/tag_a_post/:post_id/version/:id' do
+    # url_for(:account, :name => "John", :id => 5) => "/the/accounts/John/and/5"
+    # access params[:name] and params[:id]
+  end
+  get :account, :map => '/the/accounts/:name/and/:id' do
+     # url_for(:account, :name => "John", :id => 5) => "/the/accounts/John/and/5"
+     # access params[:name] and params[:id]
+   end
 
   post :create do
 
